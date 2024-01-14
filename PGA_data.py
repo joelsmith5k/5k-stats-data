ccea0f84852a7f4c9fcd443e5543f2db89c81e44
JOEL_SMITH <joelsmith5000@gmail.com>
2024-01-14 13:59:20 -0800

    add lower case name attribute to players

--- PGA_data.py
+++ PGA_data.py
@@ -34,6 +34,8 @@ def get_projections(tournament_id):
     projection_data = api_call(PGA_PROJECTIONS_BASE_URL + tournament_id)
     filtered_data = list(filter(lambda x: x['Rank'] is not None, projection_data))
     sorted_data = sorted(filtered_data, key=lambda x: x['Rank'])
+    for player in sorted_data:
+        player['name'] = player['Name']
     PLAYER_COLLECTION.delete_many({})
     PLAYER_COLLECTION.insert_many(sorted_data)
     return sorted_data
